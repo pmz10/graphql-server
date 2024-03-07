d1f10f0bc153fdc4115424306bf014a50ec941f9
pmz10 <welcome_to_pmz@hotmail.com>
2024-03-07 16:01:06 -0600

    SeverApollo

--- index.js
+++ index.js
@@ -1,53 +1,71 @@
 import { ApolloServer, gql } from "apollo-server";
+
 const persons = [
   {
     name: "Youseff",
     phone: "044-123456",
     street: "Avenida Fullstack",
     city: "Mataro",
-    id: '3d599470-3436-11e9-bc57-8b80ba54c431'
+    id: "3d599470-3436-11e9-bc57-8b80ba54c431",
   },
   {
     name: "Itzi",
     street: "Pasaje Testing",
     city: "Ibiza",
-    id: '3d599471-3436-11e9-bc57-8b80ba54c431'
+    id: "3d599471-3436-11e9-bc57-8b80ba54c431",
   },
   {
     name: "Ed Sheeran",
     phone: "044-123456",
     street: "Desvio Antesala",
     city: "Carrioli",
-    id: '3d599472-3436-11e9-bc57-8b80ba54c431'
+    id: "3d599472-3436-11e9-bc57-8b80ba54c431",
   },
 ];
 
 const typeDefinitions = gql`
-  type Person {
-    name: String!
-    phone: Int
+  type Address {
     street: String!
     city: String!
-    id: String!
+  }
+
+  type Person {
+    name: String!
+    phone: String
+    address: Address!
+    id: ID!
   }
 
   type Query {
     personCount: Int!
     allPersons: [Person]!
+    findPerson(name: String!): Person
   }
-`
+`;
 const resolvers = {
-    Query: {
-        personCount: () => persons.length,
-        allPersons: () => persons
-    }
-}
+  Query: {
+    personCount: () => persons.length,
+    allPersons: () => persons,
+    findPerson: (root, args) => {
+      const { name } = args;
+      return persons.find((person) => person, name === name);
+    },
+  },
+  Person: {
+    address: (root) => {
+      return {
+        street: root.street,
+        city: root.city,
+      };
+    },
+  },
+};
 
 const server = new ApolloServer({
-    typeDefs: typeDefinitions,
-    resolvers
-})
+  typeDefs: typeDefinitions,
+  resolvers,
+});
 
-server.listen().then(({url}) => {
-    console.log(`Sever ready at ${url}`)
-})
+server.listen().then(({ url }) => {
+  console.log(`Sever ready at ${url}`);
+});
